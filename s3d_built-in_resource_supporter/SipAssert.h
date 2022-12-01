bbcec9dfa5708ded15aba8aa971d92aba72037ac
Inoue_Satsuki <s.inoue@otwo.jp>
2022-12-01 15:58:56 +0900

    Update SipAssert.h

--- s3d_built-in_resource_supporter/SipAssert.h
+++ s3d_built-in_resource_supporter/SipAssert.h
@@ -4,56 +4,21 @@
 
 #include <Siv3D.hpp>
 
-#ifdef _WINDOWS
-#include <Windows.h>
-#include <consoleapi2.h>
-#include <consoleapi3.h>
-#endif
-
 namespace sip
 {
-
-#ifdef _WINDOWS
-
-	static inline void ChangeConsoleColorRed(void)
-	{
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
-	}
-
-	static inline void ChangeConsoleColorWhite(void)
-	{
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-	}
-
-	static inline void ChangeConsoleColorYellow(void)
-	{
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-	}
-
 	static inline int soft_assert(const char* filename, int line)
 	{
-		ChangeConsoleColorRed();
-		String fmt = U"#################### !! ASSERT !! ####################\n"
-					 U" file : {} ( line : {} )"_fmt(Unicode::Widen(filename), line);
+		String fmt = U"\n#################### !! ASSERT !! ####################\n"
+					 U" file : {} ( line : {} )\n"_fmt(Unicode::Widen(filename), line);
 		Console << fmt;
-		ChangeConsoleColorWhite();
+		// 強制ブレイクポイント
+#ifdef _WINDOWS
 		__debugbreak();
-		return 1;
-	}
-
-#else
-
-	static inline int soft_assert(const char*, int)
-	{
-		String fmt = U"#################### !! ASSERT !! ####################\n"
-					 U" file : {} ( line : {} )"_fmt(Unicode::Widen(filename), line);
-		Console << fmt;
+#else //_WINDOWS
 		for (;;) { NULL; }
+#endif//_WINDOWS
 		return 1;
 	}
-
-#endif
-
 }
 
 #ifdef _DEBUG
